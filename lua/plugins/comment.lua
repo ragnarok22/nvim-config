8096597eaa7ef8789d5f7f1143ee02f6f9cf1641
Reinier Hernández <sasuke.reinier@gmail.com>
2024-11-25 15:37:03 -0300

    perf: clean my custom configuration

--- lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
--- "gc" to comment visual regions/lines
-return {
-  'numToStr/Comment.nvim',
-  event = { "BufReadPre", "BufNewFile" },
-  config = true,
-  opts = {
-    -- add any options here
-  },
-}
