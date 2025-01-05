7af10b033e4e25550d46c7ff2da24eb85f6cca5f
Reinier Hernández <sasuke.reinier@gmail.com>
2025-01-05 13:53:12 -0300

    perf: clean the setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,19 +1,3 @@
 return {
-  "folke/neodev.nvim",  -- Keymaps hints
-  "github/copilot.vim",
-
-  "tpope/vim-surround",
-
-  -- Git related plugins
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",  -- GitHub related
-
-  "mfussenegger/nvim-dap",  -- Debug Adapter Protocol
-
-  {
-    "ragnarok22/whereami.nvim",
-  },
-  { "mistricky/codesnap.nvim", build = "make" },
-  -- integrate tmux with nvim
-  { "christoomey/vim-tmux-navigator", event = "BufReadPre" },
+  "ragnarok22/whereami.nvim",
 }
