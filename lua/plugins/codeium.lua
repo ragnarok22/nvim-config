d731f4522ed142264605e96bcb647e3f34a0f81d
Reinier Hernández <sasuke.reinier@gmail.com>
2025-02-10 11:54:50 -0300

    feat(plugins): update dependencies and enhance codeium config

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,11 +1,15 @@
 return {
   'Exafunction/codeium.nvim',
-  event = 'BufEnter',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'hrsh7th/nvim-cmp',
   },
   config = function()
-    require('codeium').setup {}
+    require('codeium').setup {
+      enable_cmp_source = false,
+      virtual_text = {
+        enabled = true,
+      },
+    }
   end,
 }
