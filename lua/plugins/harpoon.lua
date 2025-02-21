c382156de98583b8a954bdbcb3618f1445bad5b9
Reinier Hernández <sasuke.reinier@gmail.com>
2025-02-21 08:28:50 -0300

    feat(harpoon): update to use new API and keybindings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,20 +1,38 @@
 return {
   'theprimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local mark = require 'harpoon.mark'
-    local ui = require 'harpoon.ui'
+    local harpoon = require 'harpoon'
 
-    vim.keymap.set('n', '<C-b>', mark.add_file)
-    vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+    harpoon:setup()
 
-    vim.keymap.set('n', '<leader>d', function()
-      ui.nav_file(1)
+    vim.keymap.set('n', '<C-b>', function()
+      harpoon:list():add()
+    end, { desc = 'Add file to Harpoon' })
+
+    vim.keymap.set('n', '<C-e>', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Open Harpoon menu' })
+
+    vim.keymap.set('n', '<C-a>', function()
+      harpoon:list():select(1)
     end, { desc = 'Go to First Harpoon file' })
-    vim.keymap.set('n', '<leader>s', function()
-      ui.nav_file(2)
+    vim.keymap.set('n', '<C-s>', function()
+      harpoon:list():select(2)
     end, { desc = 'Go to Second Harpoon file' })
-    vim.keymap.set('n', '<leader>a', function()
-      ui.nav_file(3)
+    vim.keymap.set('n', '<C-d>', function()
+      harpoon:list():select(3)
     end, { desc = 'Go to Third Harpoon file' })
+    vim.keymap.set('n', '<C-f>', function()
+      harpoon:list():select(4)
+    end, { desc = 'Go to Fourth Harpoon file' })
+
+    vim.keymap.set('n', '<S-Tab>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<Tab>', function()
+      harpoon:list():next()
+    end)
   end,
 }
