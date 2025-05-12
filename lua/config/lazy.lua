eba8796ab0fb5f627f52f00fe4428c9011daa27b
Reinier Hernández <sasuke.reinier@gmail.com>
2025-05-12 14:00:26 -0300

    refactor(config): simplify lazy.nvim setup and error handling

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -4,16 +4,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
+    error('Error cloning lazy.nvim:\n' .. out)
   end
 end
-vim.opt.rtp:prepend(lazypath)
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
@@ -34,7 +30,21 @@ require('lazy').setup({
   checker = { enabled = false },
 }, {
   ui = {
-    icons = {},
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
   },
 })
 
