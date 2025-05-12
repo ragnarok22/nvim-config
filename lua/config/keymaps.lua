5fdda4949d5d000cca9baccbf94f9197264aa6de
Reinier Hernández <sasuke.reinier@gmail.com>
2025-05-12 13:55:33 -0300

    fix(keymaps): disable conflicting window navigation keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,11 +32,13 @@ vim.keymap.set('n', '<leader>Y', '"+Y', { desc = '[Y]ank line to system' })
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
+-- INFO: I disable this because some conflicts with vim-tmux-navigator
+--
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Split resize
 vim.keymap.set('n', '<leader>>', '10 <C-W>>', { desc = 'Increase split size' })
