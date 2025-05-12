51f37e9a7ad9720932e0ee8b6879ae3236bfb412
Reinier Hernández <sasuke.reinier@gmail.com>
2025-05-12 14:45:33 -0300

    fix(autocompletion): change LuaSnip preset from default to enter

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -56,7 +56,7 @@ return { -- Autocompletion
       -- <c-k>: Toggle signature help
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'default',
+      preset = 'enter',
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
