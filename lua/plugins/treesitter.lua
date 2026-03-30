8c6791f517ffa30127e21f6f927e77f34aa0eb53
Reinier Hernández <sasuke.reinier@gmail.com>
2026-03-30 11:19:07 -0300

    chore(treesitter): disable lazy loading

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
